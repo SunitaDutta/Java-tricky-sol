1bf2d3a45076718f81df9dc3049b86369ce1eb6c
SunitaDutta <49290085+SunitaDutta@users.noreply.github.com>
2023-05-12 15:38:13 +0530

    Int or Integer method parameter added

---
+++ Java-tricky/src/com/tricky/IntIntegerDemo.java
@@ -0,0 +1,28 @@
+public class IntIntegerDemo {
+
+	public static void main(String[] args) {
+		
+		IntIntegerDemo d = new IntIntegerDemo();
+		
+		d.print(0);
+
+	}
+	
+	
+	public void print(int n) {
+		System.out.println("INT");
+	}
+	
+	public void print(Integer n) {
+		System.out.println("Integer");
+	}
+	
+	public void print(long n) {
+		System.out.println("long");
+	}
+	
+	public void print(Long n) {
+		System.out.println("Long");
+	}
+
+}
